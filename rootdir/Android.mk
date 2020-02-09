7570b8c373bbd78be5f961e606deabfffc53c5eb
Jack Pham <jackp@codeaurora.org>
2020-02-09 00:32:01 -0800

    sdm660-common: init.qcom.usb.rc: Prepare for removal
    Guard LOCAL_MODULE definitions for init.qcom.usb.{rc,sh}
    and init.msm.usb.configfs.rc to prepare for them getting
    moved to the USB project area and defined in Android.bp
    there. Full removal of the actual files will follow once
    they are in place at the new location.
    
    In addition, move the import of init.msm.usb.configfs.rc
    to init.qcom.usb.rc to remove a direct reference from
    init.qcom.rc. This will make it easier to consolidate
    the files later after they have moved.
    
    Change-Id: I03c1bb2a148db679332ba3e0a87d8f6ad7ce1ad1

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -34,6 +34,7 @@ LOCAL_SRC_FILES    := bin/init.class_main.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+ifndef MODULE.TARGET.ETC.init.qcom.usb.rc
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.usb.rc
 LOCAL_MODULE_TAGS  := optional
@@ -57,6 +58,7 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := bin/init.qcom.usb.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
+endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.qcom.rc
