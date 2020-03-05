95dbf484a8e77d1b3dd8a43fc79cbe414b3799d7
Fenglin Wu <fenglinw@codeaurora.org>
2020-03-05 19:31:14 +0800

    jasmine_sprout : vibrator: Add config to exclude haptics devices from input subsystem
    Haptics devices which registers as input FF devices are not supposed to
    be detected by input subsystem. Add excluded-input-devices.xml file for
    input subsystem to exclude them.
    
    Change-Id: I52dc6e502b29e2813299d073195a0991bb2cf692
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Signed-off-by: raiyanbinmohsin <raiyanbinmohsinshishir@gmail.com>
    Signed-off-by: RealAkira <beastdark704@gmail.com>

--- device.mk
+++ device.mk
@@ -455,6 +455,9 @@ $(call inherit-product, build/target/product/verity.mk)
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service	
 
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
