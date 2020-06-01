52677aee3ac3549afde4691fd9efa1afb9897ca7
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2020-06-01 12:57:08 +0530

    sdm660-common: Remove deprecated android.hardware.thermal@1.0 HAL
    Remove deprecated android.hardware.thermal@1.0 HAL from
    device manifest.
    
    Bug: 154012150
    Change-Id: Ic7deb6783ecbbe13a0e09e4a040689b023529470

--- sdm660.mk
+++ sdm660.mk
@@ -343,12 +343,6 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sdm660
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
