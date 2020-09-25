984b9ccd69139ff53775c718c474333c00fdab01
StyloGey <stylogey@gmail.com>
2020-09-25 20:05:42 +0200

    sdm660-common: switch to android.hardware.boot@1.1
    Change-Id: Ia5539844acb9c2b8e92729993ef4cef34de23c64

--- sdm660.mk
+++ sdm660.mk
@@ -48,11 +48,9 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    android.hardware.boot@1.0-service \
-    bootctrl.sdm660 \
-    bootctrl.sdm660.recovery
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service
 
 # Boot control debug
 PRODUCT_PACKAGES_DEBUG += \
