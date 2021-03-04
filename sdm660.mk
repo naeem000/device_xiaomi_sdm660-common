eb06325bb93b1a44a3e2c7a578a04210b4f0130f
jimmy <quesoo@gmail.com>
2021-03-04 10:06:35 +0100

    sdm660-common: commonize torch permission
    * all supported devices have torch
    
    Signed-off-by: ElXreno <elxreno@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -248,6 +248,7 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml \
