337b80f43b3cb98404db7f45393fe4608d0ba132
Demon Singur <demonsingur@gmail.com>
2021-10-01 22:32:20 +0300

    jasmine_sprout : do not build useless package
    It is compiled as dependency of ipacm
    
    Signed-off-by: muti605 <mustafa.sahin605@yandex.com>
    Signed-off-by: RealAkira <beastdark704@gmail.com>

--- device.mk
+++ device.mk
@@ -399,8 +399,7 @@ PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
     libjson \
     libprotobuf-cpp-full \
-    librmnetctl \
-    libxml2
+    librmnetctl 
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
