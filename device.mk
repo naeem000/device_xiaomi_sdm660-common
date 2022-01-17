ad105eb540956f7f3a49b485d6159673b5a83a8c
Edwin Moquete <edwinmmoquete@gmail.com>
2022-01-17 10:13:32 +0530

    jasmine_sprout : Move thermal configs to device tree
    Signed-off-by: RealAkira <beastdark704@gmail.com>

--- device.mk
+++ device.mk
@@ -420,6 +420,13 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
+    
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
+    $(DEVICE_PATH)/configs/thermal-engine-map.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-map.conf \
+    $(DEVICE_PATH)/configs/thermal-engine-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-normal.conf \
+    $(DEVICE_PATH)/configs/thermal-engine-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-video.conf    
 
 # Trust
 PRODUCT_PACKAGES += \
