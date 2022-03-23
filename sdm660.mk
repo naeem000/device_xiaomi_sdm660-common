4217eca5050a8f51ed72c4951dc478e4b1954ad3
David Li <dvdli@google.com>
2022-03-23 23:33:25 +0800

    sdm660-common: Upgrade audio.usb.default from v1 to v2
    Bug: 187912470
    Test: make -j128
    Test: deep buffer and compress offload via speaker, USB headset, BT
    earphone
    Test: phone call via handset
    Test: hotword detection

--- sdm660.mk
+++ sdm660.mk
@@ -82,6 +82,7 @@ PRODUCT_PACKAGES += \
     audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
+    audio.usbv2.default \
     libaudio-resampler \
     libhdmiedid \
     libhfp \
